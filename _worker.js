7d364ab2f94ad7bd383e8edec7165ee99e03dd40
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-05-28 11:50:39 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,6 @@ export default {
     const domain = url.hostname === 't.bibica.net' ? 't.me' : 'telegram.org';
     const targetUrl = `https://${domain}${url.pathname}${url.search}`;
 
-    // Clone request headers và sửa Host header
     const headers = new Headers(request.headers);
     headers.set('Host', domain);
     headers.delete('Referer');
@@ -14,17 +13,13 @@ export default {
       redirect: 'follow'
     });
 
-    // Nếu là HTML thì sửa lại các link tài nguyên
     if (response.headers.get('content-type')?.includes('text/html')) {
       let html = await response.text();
       
-      // Thay thế tất cả link tài nguyên
       html = html.replace(/(https?:)?\/\/(t\.me|telegram\.org)/g, 'https://t.bibica.net');
       
-      // Clone response và sửa headers
       const modifiedResponse = new Response(html, response);
-      
-      // Sửa CSP header để cho phép load từ domain của bạn
+
       const csp = modifiedResponse.headers.get('content-security-policy') || '';
       modifiedResponse.headers.set(
         'content-security-policy', 
@@ -33,8 +28,7 @@ export default {
       
       return modifiedResponse;
     }
-
-    // Với các file tĩnh (CSS, JS...) thì trả về trực tiếp
+    
     return response;
   }
 };
