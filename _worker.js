b88b21a7bfa3667d308b4439d2417a25acf869c0
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-05-31 02:22:55 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -16,10 +16,15 @@ export default {
     if (response.headers.get('content-type')?.includes('text/html')) {
       let html = await response.text();
       
-      html = html.replace(/(https?:)?\/\/(t\.me|telegram\.org)/g, 'https://t.bibica.net');
+      // Thay thế tất cả các liên kết tg:// thành https://t.bibica.net/
+      html = html.replace(/tg:\/\/([a-zA-Z0-9_\-?=&]+)/g, 'https://t.bibica.net/$1');
+      
+      // Thay thế tất cả các liên kết t.me/ thành https://t.bibica.net/
+      html = html.replace(/(https?:)?\/\/t\.me\/([a-zA-Z0-9_\/\-?=&]+)/g, 'https://t.bibica.net/$2');
       
       const modifiedResponse = new Response(html, response);
 
+      // Sửa CSP để tránh bị chặn
       const csp = modifiedResponse.headers.get('content-security-policy') || '';
       modifiedResponse.headers.set(
         'content-security-policy', 
